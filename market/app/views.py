1b62a10a2255596ad4615999c60ff87ff4ca2f94
Sergey Tyukin <STyukin@gmail.com>
2020-02-01 22:33:09 +0300

    Initial commit

---
+++ market/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def cart_view(request):
+    return render(request, 'app/cart.html', {})
+
+
+def empty_secition_view(request):
+    return render(request, 'app/empty_secition.html', {})
+
+
+def index_view(request):
+    return render(request, 'app/index.html', {})
+
+
+def login_view(request):
+    return render(request, 'app/login.html', {})
+
+
+def phone_view(request):
+    return render(request, 'app/phone.html', {})
+
+
+def smartphones_view(request):
+    return render(request, 'app/smartphones.html', {})
